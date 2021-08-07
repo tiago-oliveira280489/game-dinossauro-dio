f42319a3419fdae4bad59ff36fada8653a6466bf
Tiago Antônio Borba Oliveira <tiago.engcomp89@gmail.com>
2021-08-07 15:03:54 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+const dino = document.querySelector('.dino');
+let isJumping = false;
+const background = document.querySelector('.background');
+let position = 0;
+let isGameOver = false;
+
+function handleKeyDown(event) {
+    if (event.keyCode === 32) {
+        if (!isJumping) {
+            jump();
+        }
+    }
+}
+
+function jump() {
+    isJumping = true;
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            //Descida
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            //Subida
+            position += 20;
+            dino.style.bottom = position + 'px';
+        }
+    }, 20);
+}
+
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    if (isGameOver) return;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = cactusPosition + 'px';
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //gameOver
+            clearInterval(leftInterval);
+            isGameOver = true;
+            document.body.innerHTML = '<h1 class="gameOver">Fim de Jogo!<br>Aperte F5 para recomeçar</h1>';
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
+}
+
+createCactus();
+
+document.addEventListener('keydown', handleKeyDown);
